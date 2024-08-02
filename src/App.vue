a903121b7ecf574d9de4b9bfaaf4f7525b6f42e1
bezawit11234 <beza.mege@gmail.com>
2024-08-02 01:34:30 -0700

    first uplod

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1 class="text-3x1 text-golden">Hello world</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        golden: '#FFD700', // Define the golden color
+      },
+    },
+  },
+  plugins: [],
+}
